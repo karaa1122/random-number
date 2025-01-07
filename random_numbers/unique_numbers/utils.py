86a6fbf1c25b2b94258d8e280d6f0fa504b3ec1d
karaa <karaa.kamaran@gmail.com>
2025-01-07 10:24:25 +0300

    Add sarcastic messages

--- random_numbers/unique_numbers/utils.py
+++ random_numbers/unique_numbers/utils.py
@@ -12,6 +12,26 @@ SARCASTIC_MESSAGES = [
     "This number is yours forever. Aren’t you special?",
     "You clicked a button. Groundbreaking stuff.",
     "Keep going; maybe you’ll find meaning in this. Or not.",
+    "Here’s a number that’s about as exciting as watching paint dry.",
+    "A truly groundbreaking achievement. Give yourself a pat on the back.",
+    "Another number, because why not waste a few more seconds of your life?",
+    "This is the pinnacle of human achievement… or not.",
+    "Behold! A number that will totally change your life. (Spoiler: it won’t.)",
+    "A life-altering number, assuming your life is already pretty boring.",
+    "Another step closer to absolutely nothing of significance.",
+    "You’ve unlocked… a number. I’m sure the world is so grateful.",
+    "Your quest for uniqueness has led to… this. Well done?",
+    "This number is as unique as the way you waste your time.",
+    "You’ve done it! You’ve achieved the impossible: mediocrity.",
+    "A special number, for a not-so-special occasion.",
+    "Don’t get too excited. It’s just a number, not a lottery win.",
+    "Celebrate! You’re now the proud owner of a meaningless integer.",
+    "Your persistence has been rewarded with… a number. Wow.",
+    "Here’s your number. Try not to get emotional about it.",
+    "Brace yourself, this number is going to blow your mind. Or not.",
+    "You’ve reached peak productivity: clicking for a random number.",
+    "This number is so unique, even your dog wouldn’t care.",
+    "Another unique number for your collection of things that don’t matter."
 ]
 
 def generate_unique_number_and_message():
